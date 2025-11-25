8964c247ec5a30e1164b425446b86badc1ae40b3
rhslxkd <ide1125@cau.ac.kr>
2025-11-25 17:21:19 +0900

    feat:유저 더미 데이터 추가-> 다른 기능들과의 연결

---
+++ LongLife/src/main/java/com/oraclejava/longlife/model/Users.java
@@ -0,0 +1,25 @@
+package com.oraclejava.longlife.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Users {
+    @Id
+    @Column(name = "user_id")
+    private String userId;
+    private String email;
+    private String name;
+    private String password;
+    private String address;
+    private int height;
+    private int weight;
+    private LocalDateTime regdate;
+}
