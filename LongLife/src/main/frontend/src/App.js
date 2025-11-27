4d668b069c0a69d7ed7a65cc6cc52eedd44c2202
rhslxkd <ide1125@cau.ac.kr>
2025-11-27 11:53:31 +0900

    chore:dev파일에 병합

--- LongLife/src/main/frontend/src/App.js
+++ LongLife/src/main/frontend/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import {Route, Routes} from "react-router-dom";
-import AiChatPage from "./pages/AiChatPage";
+import AiChatPage from "./longlife/ai/AiChatPage";
 
 
 function App() {
