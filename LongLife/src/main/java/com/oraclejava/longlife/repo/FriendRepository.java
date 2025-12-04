8b65fc59ae5040c30cf83105470d5a12c4b606d2
sangjin <>
2025-12-04 16:07:37 +0900

    feat: 친구 목록, 요청, 수락, 삭제 기능 추가

---
+++ LongLife/src/main/java/com/oraclejava/longlife/repo/FriendRepository.java
@@ -0,0 +1,14 @@
+package com.oraclejava.longlife.repo;
+
+import com.oraclejava.longlife.model.Friend;
+import com.oraclejava.longlife.model.FriendStatus;
+import com.oraclejava.longlife.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FriendRepository extends JpaRepository<Friend, Long> {
+    List<Friend> findByRequesterAndStatus(Users requester, FriendStatus status);
+    List<Friend> findByReceiverAndStatus(Users receiver, FriendStatus status);
+    List<Friend> findByReceiverUserIdAndStatus(String receiverUserId, FriendStatus status);
+}
